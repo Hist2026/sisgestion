220142921475ca35f8258c3836dd578b5ead773d
gerardosss <santo__619@gmail.com>
2026-02-11 00:01:38 -0400

    form

--- app/Http/Controllers/ConfiguracionController.php
+++ app/Http/Controllers/ConfiguracionController.php
@@ -14,8 +14,11 @@ class ConfiguracionController extends Controller
 
     public function index(){
 
+
+            $jsonData = file_get_contents('https://api.hilariweb.com/divisas');
+            $divisas = json_decode($jsonData,true);
             $configuracion = Configuracion::first();
-        return view('admin.configuracion.index', compact('configuracion'));
+        return view('admin.configuracion.index', compact('configuracion','divisas'));
 
 
     }
--- resources/views/admin/configuracion/index.blade.php
+++ resources/views/admin/configuracion/index.blade.php
@@ -23,45 +23,115 @@
         <form action="">
 
                 <div class="row">
-                            <div class="col-md-4">
+                    <div class="col-md-4">
+                         dasdas
+                    </div>
 
-
-
-                            </div>
-
-                             <div class="col-md-8">
-
-                               <div class="row">
-                                    <div class="col-md-4">
-                                        dasdas
-
-                                    </div>
-
+                    <div class="col-md-8">
+                        <div class="row">
+                                <div class="col-md-4">
                                     <div class="form-group">
-
                                             <label for="for">nombre</label>
-                                
-                                            
                                                 <div class="input-group mb-3">
                                                     <div class="input-group-prepend">
-                                                        <span> <i class="fas fa-university"></i></span>
+                                                        <span class="input-group-text"> <i class="fas fa-university"></i></span>
                                                        
                                                     </div>
-
-                                                </div>
                                                 <input type="text" class="form-control" value="{{ old('nombre', $configuracion->nombre ?? '') }} " name="nombre" placeholder="escribe tu nombre" required>
+                                                </div>
                                    
                                         @error('nombre')
                                             <small style="color: red">{{ $message}}</small>
                                         @enderror
+                                    </div>
+                                </div>
+                           
+
+                            
+                         
+                             <div class="col-md-8">
+                              
                                    
-                                            </div>
-
+                                    <div class="form-group">
+                                            <label for="for">Descripcion</label>
+                                                <div class="input-group mb-3">
+                                                    <div class="input-group-prepend">
+                                                        <span class="input-group-text"> <i class="fas fa-align-left"></i></span>
+                                                       
+                                                    </div>
+                                                <input type="text" class="form-control" value="{{ old('descripcion', $configuracion->descripcion ?? '') }} " name="descripcion" placeholder="escribe tu descripcion" required>
+                                                </div>
+                                   
+                                        @error('descripcion')
+                                            <small style="color: red">{{ $message}}</small>
+                                        @enderror
+                                    </div>
                                </div>
+                        </div>
+                        <div class="row">
 
-                            </div>
+                                <div class="col-md-6">
+                                        <div class="form-group">
+                                                <label for="for">direccion</label>
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text"> <i class="fas fa-university"></i></span>
+                                                        
+                                                        </div>
+                                                    <input type="text" class="form-control" value="{{ old('direccion', $configuracion->direccion ?? '') }} " name="direccion" placeholder="escribe tu direccion" required>
+                                                    </div>
+                                    
+                                            @error('direccion')
+                                                <small style="color: red">{{ $message}}</small>
+                                            @enderror
+                                        </div>
+                                </div>
+                               <div class="col-md-3">
+                                        <div class="form-group">
+                                                <label for="for">telefono</label>
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text"> <i class="fas fa-phone"></i></span>
+                                                        
+                                                        </div>
+                                                    <input type="text" class="form-control" value="{{ old('telefono', $configuracion->telefono ?? '') }} " name="telefono" placeholder="escribe tu telefono" required>
+                                                    </div>
+                                    
+                                            @error('telefono')
+                                                <small style="color: red">{{ $message}}</small>
+                                            @enderror
+                                        </div>
+                                </div>
+
+                                    <div class="col-md-3">
+                                        <div class="form-group">
+                                                <label for="for">divisa</label>
+                                                    <div class="input-group mb-3">
+                                                        <div class="input-group-prepend">
+                                                            <span class="input-group-text"> <i class="fas fa-money-bill-wave"></i></span>
+                                                        
+                                                        </div>
+                                                            <select name="divisa" class="form-control" required>
+
+                                                            <option value="">SELECCIONE UNA OPCION</option>
+                                                            @foreach($divisas as $divisa )
+                                                            <option value="{{ $divisa['symbol']  }}" {{old('divisa', $configuracion->divisa ?? '') == $divisa['symbol'] ? 'selected' : '' }}> {{$divisa['name'] }} </option>
+
+                                                            @endforeach
+                                                            </select>                                                    
+                                                    </div>
+                                    
+                                            @error('divisa')
+                                                <small style="color: red">{{ $message}}</small>
+                                            @enderror
+                                        </div>
+                                </div>
 
 
+
+                        </div>
+
+                     </div>
                 </div>
         </form>
       </div>
